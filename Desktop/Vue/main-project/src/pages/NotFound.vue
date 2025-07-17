5881a1a0b4b87ee80285b01c11474ec52d5f0fd9
Sema-Husiyeva <semahusiyeva64@gmail.com>
2025-07-17 23:33:26 +0400

    Delete Desktop/Vue directory
    Delete Vue directory

--- Desktop/Vue/main-project/src/pages/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <section>
-        <base-card>
-        <h2>Page not found</h2>
-        <p>Check out all our <router-link to="/coaches">Coaches</router-link></p>
-    </base-card>
-    </section>
-</template>
